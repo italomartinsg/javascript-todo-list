cce903135759d894ab96ab6659ea2c999d627e96
Ítalo Martins <italomartinsg@gmail.com>
2026-08-24 18:54:52 -0300

    feat: render tasks in the DOM

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@ const form = document.querySelector("form");
 const input = document.querySelector(".texto");
 const ulTask = document.querySelector(".task-list");
 
-const taskList = [];
+//modelo de objetos para teste
+// {id: 1, texto: 'JS',concluida: false}
+
+const taskList = [
+  { id: 1, texto: "JavaScript", concluida: false },
+  { id: 2, texto: "HTML", concluida: false },
+  { id: 3, texto: "CSS", concluida: false },
+];
 function gerarId(taskList) {
   const maiorID = taskList.reduce((acumulador, atual) => {
     if (atual.id > acumulador) {
@@ -26,9 +33,21 @@ form.addEventListener("submit", (event) => {
     };
 
     taskList.push(task);
+    renderizarTarefas(taskList);
     console.log(taskList);
   } else {
     console.log("texto inválido, entrando no return");
     return;
   }
 });
+
+function renderizarTarefas(taskList) {
+  ulTask.textContent = "";
+  taskList.forEach((task) => {
+    const newLi = document.createElement("li");
+    newLi.textContent = task.texto;
+    ulTask.appendChild(newLi);
+  });
+}
+
+renderizarTarefas(taskList);
