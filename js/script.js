5f7637a83509277c16460c2dba7ba7531a04722d
Ítalo Martins <italomartinsg@gmail.com>
2026-08-24 20:43:43 -0300

    feat: toggle task completion

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ const input = document.querySelector(".texto");
 const ulTask = document.querySelector(".task-list");
 
 //modelo de objetos para teste
-// {id: 1, texto: 'JS',concluida: false}
+//  { id: 1, texto: "Estudar JavaScript", concluida: false },  { id: 2, texto: "Estudar DOM", concluida: false },  { id: 3, texto: "Praticar Arrays", concluida: false }
 
 const taskList = [
-  { id: 1, texto: "JavaScript", concluida: false },
-  { id: 2, texto: "HTML", concluida: false },
-  { id: 3, texto: "CSS", concluida: false },
+  { id: 1, texto: "Estudar JavaScript", concluida: false },
+  { id: 2, texto: "Estudar DOM", concluida: false },
+  { id: 3, texto: "Praticar Arrays", concluida: false },
 ];
 function gerarId(taskList) {
   const maiorID = taskList.reduce((acumulador, atual) => {
@@ -20,6 +20,20 @@ function gerarId(taskList) {
 
   return maiorID + 1;
 }
+function renderizarTarefas(taskList) {
+  ulTask.textContent = "";
+  taskList.forEach((task) => {
+    const newLi = document.createElement("li");
+    newLi.textContent = task.texto;
+    newLi.setAttribute("data-id", task.id);
+    if (task.concluida) {
+      newLi.classList.add("concluida");
+    }
+    ulTask.appendChild(newLi);
+  });
+}
+
+renderizarTarefas(taskList);
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -33,21 +47,20 @@ form.addEventListener("submit", (event) => {
     };
 
     taskList.push(task);
+    input.value = "";
     renderizarTarefas(taskList);
-    console.log(taskList);
   } else {
     console.log("texto inválido, entrando no return");
     return;
   }
 });
 
-function renderizarTarefas(taskList) {
-  ulTask.textContent = "";
-  taskList.forEach((task) => {
-    const newLi = document.createElement("li");
-    newLi.textContent = task.texto;
-    ulTask.appendChild(newLi);
-  });
-}
+ulTask.addEventListener("click", (event) => {
+  const clickDataSet = +event.target.dataset.id;
 
-renderizarTarefas(taskList);
+  const searchTask = taskList.find((task) => task.id === clickDataSet);
+
+  searchTask.concluida = !searchTask.concluida;
+
+  renderizarTarefas(taskList);
+});
